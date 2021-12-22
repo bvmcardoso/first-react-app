72fe5652002e564e819fb0b00841697040b910d7
Bruno Vinicius M Cardoso <bruno.cardoso@iclinic.com.br>
2021-12-22 16:23:32 -0300

    functional component - increment/decrement using state

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,43 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import CustomerList from './CustomerList'
 import {
     Component
 } from 'react';
 
-function App() {
+import Counter from './Counter.js'
 
-    const customers = ['Andrew', 'Carnegie', 'John', 'Mat', 'Natan']
+class App extends Component {
 
-    return (
-        <div>
-            <h1>App</h1>
-            <CustomerList allCustomers={customers} total="4" test="New prop"/>
-        </div>
-    )
+    render() {
+        return (
+            <div>
+            <h1>Hello world</h1>
+                <Counter />
+            </div>
+        )
+    }
 }
 
-// class App extends Component {
-
-//     constructor() {
-//         super()
-//         console.log("[constructor]")
-//     }
-
-//     componentDidMount() {
-//         // Called when the component is mounted on the DOM
-//         console.log('[componentDidMount]')
-//     }
-
-//     componentDidUpdate() {
-//         // Called when the property has been changed or when the state is changed
-//         console.log('[componentDidUpdate]')
-//     }
-
-//     render() {
-//         console.log("[render]")
-//         return <h1> Hello world </h1>
-//     }
-// }
-
-export default App;
+export default App
