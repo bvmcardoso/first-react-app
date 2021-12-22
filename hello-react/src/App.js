ccfb90900f1920c1e7148fcd29761a31ee4cf122
Bruno Vinícius <bvmcardoso@gmail.com>
2021-12-22 20:45:57 -0300

    adding custom elements and getting them via state

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -3,13 +3,27 @@ import './App.css';
 import Counter from './Counter';
 import Stepper from './Stepper'
 
+import { useState } from 'react'
+
 function App() {
+
+    const [user, setUser] = useState({})
+
+    const handleChange = (e) => {
+        setUser({
+            ...user,   //Spread operator
+            [e.target.name]:[e.target.value]
+        })
+    }
+
     return (
-        <div className="App">
-            App component
-            <Stepper />
+        <div className="login">
+            Username: <input type="text" onChange={ handleChange } name="username"/>
+            Password: <input type="password" onChange={ handleChange } name="password" />
+            <button>Send</button>
         </div>
-    );
+
+    )
 }
 
 export default App;
